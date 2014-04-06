436f9be0462079f38ab0ac423329d1159bc9eb6d
Ben Packard <ben.packard@gmail.com>
2014-04-05 22:44:01 -0400

    Initial Commit

---
+++ BENNotificationViewDemo/BENAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BENAppDelegate.h
+//  BENNotificationViewDemo
+//
+//  Created by Ben Packard on 4/5/14.
+//  Copyright (c) 2014 Ben Packard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BENAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BENNotificationViewDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BENNotificationViewDemo
+//
+//  Created by Ben Packard on 4/5/14.
+//  Copyright (c) 2014 Ben Packard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BENAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([BENAppDelegate class]));
+	}
+}
